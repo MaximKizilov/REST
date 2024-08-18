33c500e21d8492c02ee5d4db9d161318c0b053cf
“Maxim <kizilov2016@yandex.ru>
2024-08-18 22:39:46 +1000

    one commit

--- src/main/java/ru/netology/repository/UserRepository.java
+++ src/main/java/ru/netology/repository/UserRepository.java
@@ -15,9 +15,10 @@ public class UserRepository {
                     new AbstractMap.SimpleEntry<>("Petrov", "12345"), new ArrayList<>(List.of(Authorities.DELETE, Authorities.WRITE, Authorities.READ)),
                     new AbstractMap.SimpleEntry<>("Ivanov", "54321"),  new ArrayList<>(List.of(Authorities.READ)),
                     new AbstractMap.SimpleEntry<>("Sidorov", "56789"),  new ArrayList<>(List.of(Authorities.DELETE,  Authorities.READ))
+
             );
 
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return userAuthorities.get(new AbstractMap.SimpleEntry<>(user, password)).stream().toList();
+        return userAuthorities.get(new AbstractMap.SimpleEntry<>(user, password));
     }
 }
